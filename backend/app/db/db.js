ffba3154b0c676b60a9cb2a2f8c64ff8e0b676ec
PanoccaJhon <jpanoccam@unsa.edu.pe>
2024-10-29 16:03:38 -0500

    feat: API de backend funcionando

--- backend/app/db/db.js
+++ backend/app/db/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'myuser',
-  host: 'database-db-1',  // El nombre del servicio PostgreSQL en Docker
+  host: 'localhost',  // El nombre del servicio PostgreSQL en Docker
   database: 'mydatabase',
   password: 'mypassword',
   port: 5432,
--- backend/app/routes/appRoutes.js
+++ backend/app/routes/appRoutes.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const pool = require('../db/db');
 
 // Ruta para obtener todos los Pokémon
-router.get('/pokemons', async (req, res) => {
+router.get('/usuarios', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM artists');
+    const result = await pool.query('SELECT * FROM usuarios');
     res.json(result.rows);
   } catch (error) {
-    console.error('Error al obtener los pokemons:', error);
-    res.status(500).json({ error: 'Error al obtener los pokemons' });
+    console.error('Error al obtener los usuarios:', error);
+    res.status(500).json({ error: 'Error al obtener los usuarios' });
   }
 });
 
