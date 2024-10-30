ef78189bfa84a3c5798483d2ddea4819c0b07ef5
PanoccaJhon <jpanoccam@unsa.edu.pe>
2024-10-29 22:47:33 -0500

    feat: API de backend funcionando base de datos y paginacion

--- backend/app/routes/appRoutes.js
+++ backend/app/routes/appRoutes.js
@@ -2,11 +2,24 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db/db');
 
-// Ruta para obtener todos los Pokémon
+// Ruta para obtener usuarios con paginación
 router.get('/usuarios', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM usuarios');
-    res.json(result.rows);
+    // Obtener `limit` y `offset` de los parámetros de consulta, con valores predeterminados
+    const limit = parseInt(req.query.limit) || 5;  // Predeterminado a 10 elementos
+    const offset = parseInt(req.query.offset) || 0; // Predeterminado a 0 (primer elemento)
+
+    // Consulta a la base de datos con paginación
+    const result = await pool.query('SELECT * FROM usuarios LIMIT $1 OFFSET $2', [limit, offset]);
+
+    // Enviar respuesta JSON con los datos y detalles de paginación
+    res.json({
+      success: true,
+      data: result.rows,
+      count: result.rows.length,
+      offset:offset,
+      limit:limit
+    });
   } catch (error) {
     console.error('Error al obtener los usuarios:', error);
     res.status(500).json({ error: 'Error al obtener los usuarios' });
